519d44814bada064a4ed9b483eceef805ce523d0
Yuii24 <linian2040@gmail.com>
2024-08-05 13:35:29 +0800

    test

---
+++ test.js
@@ -0,0 +1,28 @@
+var a = {}; var b = {}; var c = a
+
+if (a === b) console.log("a===b")
+
+if (a === c) console.log("a===c")
+
+
+
+
+console.log("1")
+
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(() => {
+//     console.log(i)
+//   }, 1000)
+// }
+
+// console.log("2")
+// for (let i = 0; i < 5; i++) {
+//   setTimeout(() => {
+//     console.log(i)
+//   }, 1000)
+// }
+
+
+// const a = [parseInt(3, 8), parseInt(3, 2), parseInt(3, 0)];
+// const b = [Number.min_value = 0];
+// console.log(b);
